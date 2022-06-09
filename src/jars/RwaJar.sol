1d4874a63035dff82d2310a1c5ee1db914febf54
Henrique Barcelos <16565602+hbarcelos@users.noreply.github.com>
2022-06-09 20:13:24 -0300

    feat(RwaJar): introduce SF payments directly to the SB

---
+++ src/jars/RwaJar.sol
@@ -0,0 +1,81 @@
+// Copyright (C) 2020-2021 Lev Livnev <lev@liv.nev.org.uk>
+// Copyright (C) 2021-2022 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.12;
+
+import "dss-interfaces/dss/DaiJoinAbstract.sol";
+import "dss-interfaces/dss/DaiAbstract.sol";
+
+/**
+ * @author Henrique Barcelos <henrique@clio.finance>
+ * @title RwaJar: Facility to allow stability fee payments into the Surplus Buffer.
+ * @dev Users can either send Dai directly to this conract or approve it to pull Dai from their wallet.
+ */
+contract RwaJar {
+    /// @notice The DaiJoin adapter from MCD.
+    DaiJoinAbstract public immutable daiJoin;
+    /// @notice The Dai token.
+    DaiAbstract public immutable dai;
+    /// @notice The Vow address from MCD.
+    address public immutable vow;
+
+    /**
+     * @notice Emitted whenever Dai is sent to the `vow`.
+     * @param amount The amount of Dai sent.
+     */
+    event Toss(uint256 amount);
+
+    /**
+     * @dev The Dai address is obtained from the DaiJoin contract.
+     * @param daiJoin_ The DaiJoin adapter from MCD.
+     * @param vow_ The vow from MCD.
+     */
+    constructor(address daiJoin_, address vow_) public {
+        daiJoin = DaiJoinAbstract(daiJoin_);
+        dai = DaiAbstract(DaiJoinAbstract(daiJoin_).dai());
+        vow = vow_;
+
+        DaiAbstract(DaiJoinAbstract(daiJoin_).dai()).approve(daiJoin_, type(uint256).max);
+    }
+
+    /**
+     * @notice Transfers any outstanding Dai balance in this contract to the `vow`.
+     * @dev Reverts if there Dai balance of this contract is zero.
+     * @dev This effectively burns ERC-20 Dai and credits it to the internal Dai balance of the `vow` in the Vat.
+     */
+    function void() external {
+        uint256 balance = dai.balanceOf(address(this));
+        require(balance > 0, "RwaJar/already-empty");
+
+        daiJoin.join(vow, balance);
+
+        emit Toss(balance);
+    }
+
+    /**
+     * @notice Pulls `wad` amount of Dai from the sender's wallet into the `vow`.
+     * @dev Requires `msg.sender` to have previously `approve`d this contract to spend at least `wad` Dai.
+     * @dev This effectively burns ERC-20 Dai and credits it to the internal Dai balance of the `vow` in the Vat.
+     * @param wad The amount of Dai.
+     */
+    function toss(uint256 wad) external {
+        dai.transferFrom(msg.sender, address(this), wad);
+        daiJoin.join(vow, wad);
+
+        emit Toss(wad);
+    }
+}
---
+++ src/jars/RwaJar.t.sol
@@ -0,0 +1,113 @@
+// Copyright (C) 2022 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.6.12;
+
+import {DSTest} from "ds-test/test.sol";
+import {DSToken} from "ds-token/token.sol";
+import {DSMath} from "ds-math/math.sol";
+
+import {Vat} from "dss/vat.sol";
+import {DaiJoin} from "dss/join.sol";
+
+import {RwaJar} from "./RwaJar.sol";
+
+interface Hevm {
+    function store(
+        address,
+        bytes32,
+        bytes32
+    ) external;
+}
+
+contract RwaJarTest is DSTest, DSMath {
+    bytes20 internal constant CHEAT_CODE = bytes20(uint160(uint256(keccak256("hevm cheat code"))));
+
+    Hevm internal hevm;
+
+    Vat internal vat;
+    DaiJoin internal daiJoin;
+    DSToken internal dai;
+    address internal constant VOW = address(0x1337);
+
+    RwaJar internal jar;
+
+    function setUp() public {
+        hevm = Hevm(address(CHEAT_CODE));
+
+        vat = new Vat();
+        dai = new DSToken("Dai");
+        daiJoin = new DaiJoin(address(vat), address(dai));
+
+        vat.rely(address(daiJoin));
+        dai.setOwner(address(daiJoin));
+
+        jar = new RwaJar(address(daiJoin), VOW);
+    }
+
+    function test_void_sends_all_dai_balance_to_the_vow(uint128 amount) public {
+        // Make sure amount is not zero
+        amount = (amount % (type(uint128).max - 1)) + 1;
+
+        _createFakeDai(address(this), amount);
+        dai.transfer(address(jar), amount);
+
+        jar.void();
+
+        assertEq(dai.balanceOf(address(jar)), 0, "Balance of RwaJar is not zero");
+        assertEq(vat.dai(VOW), _rad(amount), "Vow internal balance not equals to the amount transfereed");
+    }
+
+    function testFail_void_when_dai_balance_is_zero() public {
+        jar.void();
+    }
+
+    function test_toss_pulls_dai_from_sender_into_the_vow(uint128 amount) public {
+        // Make sure amount is not zero
+        amount = (amount % (type(uint128).max - 1)) + 1;
+
+        _createFakeDai(address(this), amount);
+        dai.approve(address(jar), amount);
+
+        uint256 senderBalanceBefore = dai.balanceOf(address(this));
+        uint256 vowBalanceBefore = vat.dai(VOW);
+
+        jar.toss(amount);
+
+        uint256 senderBalanceAfter = dai.balanceOf(address(this));
+        uint256 vowBalanceAfter = vat.dai(VOW);
+
+        assertEq(senderBalanceAfter, senderBalanceBefore - amount, "Balance of sender not reduced correctly");
+        assertEq(vowBalanceAfter, vowBalanceBefore + _rad(amount), "Balance of vow not increased correctly");
+    }
+
+    function _createFakeDai(address usr, uint256 wad) private {
+        // Set initial balance for `usr` in the vat
+        hevm.store(address(vat), keccak256(abi.encode(usr, 5)), bytes32(_rad(wad)));
+        // Authorizes daiJoin to operate on behalf of the user in the vat
+        hevm.store(
+            address(vat),
+            keccak256(abi.encode(address(daiJoin), keccak256(abi.encode(usr, 1)))),
+            bytes32(uint256(1))
+        );
+        // Converts the minted Dai into ERC-20 Dai and sends it to `usr`.
+        daiJoin.exit(usr, wad);
+    }
+
+    function _rad(uint256 wad) internal pure returns (uint256) {
+        return mul(wad, RAY);
+    }
+}

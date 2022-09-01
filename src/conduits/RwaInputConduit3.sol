671285d132f217dee27cbdc25c5a7a7be1e094f2
Nazar Duchak <nazar@iovlabs.org>
2022-09-01 18:53:02 +0300

    feat: fix conduits comments

--- src/conduits/RwaInputConduit3.sol
+++ src/conduits/RwaInputConduit3.sol
@@ -108,19 +108,13 @@ contract RwaInputConduit3 {
      * @notice Define addresses and gives `msg.sender` admin access.
      * @param _psm PSM contract address.
      * @param _to RwaUrn contract address.
-     * @param _quitTo Address to where outstanding GEM balance will go after `quit`
      */
-    constructor(
-        address _psm,
-        address _to,
-        address _quitTo
-    ) public {
+    constructor(address _psm, address _to) public {
         DSTokenAbstract _gem = DSTokenAbstract(GemJoinAbstract(PsmAbstract(_psm).gemJoin()).gem());
         psm = PsmAbstract(_psm);
         dai = DSTokenAbstract(PsmAbstract(_psm).dai());
         gem = _gem;
         to = _to;
-        quitTo = _quitTo;
 
         // Give unlimited approve to PSM gemjoin
         _gem.approve(address(PsmAbstract(_psm).gemJoin()), type(uint256).max);
@@ -201,7 +195,7 @@ contract RwaInputConduit3 {
      * @dev `msg.sender` must have received push access through `mate()`.
      */
     function push() external isMate {
-        _doPush(gem.balanceOf(address(this)), 0);
+        _doPush(gem.balanceOf(address(this)));
     }
 
     /**
@@ -210,7 +204,7 @@ contract RwaInputConduit3 {
      * @param amt Gem amount.
      */
     function push(uint256 amt) external isMate {
-        _doPush(amt, dai.balanceOf(address(this)));
+        _doPush(amt);
     }
 
     /**
@@ -231,11 +225,21 @@ contract RwaInputConduit3 {
     }
 
     /**
+     * @notice Flushes out all outstandingDAI balance to `usr` address.
+     * @dev Can be called only by admin.
+     * @param usr Destination address.
+     */
+    function quitDai(address usr) external auth {
+        dai.transfer(usr, dai.balanceOf(address(this)));
+    }
+
+    /**
      * @notice Swaps the specified amount of GEM into DAI through the PSM and push it into the `to` address.
      * @param amt GEM amount.
-     * @param prevDaiBalance Previous DAI balance used to track exact amount of GEM swapped for DAI in the PSM. Set to `0` if you want to get all outstanding DAI balance.
      */
-    function _doPush(uint256 amt, uint256 prevDaiBalance) internal {
+    function _doPush(uint256 amt) internal {
+        uint256 prevDaiBalance = dai.balanceOf(address(this));
+
         psm.sellGem(address(this), amt);
 
         uint256 daiBalance = dai.balanceOf(address(this));
@@ -250,6 +254,7 @@ contract RwaInputConduit3 {
      * @param amt GEM amount.
      */
     function _doQuit(uint256 amt) internal {
+        require(quitTo != address(0), "RwaInputConduit3/invalid-quit-to-address");
         gem.transfer(quitTo, amt);
         emit Quit(quitTo, amt);
     }

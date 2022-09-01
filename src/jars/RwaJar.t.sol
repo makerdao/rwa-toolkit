355af038df98cbeac890600a21c263f320f8754b
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2022-09-01 17:09:53 -0300

    refactor: remove commented out code

--- src/jars/RwaJar.t.sol
+++ src/jars/RwaJar.t.sol
@@ -98,7 +98,6 @@ contract RwaJarTest is Test, DSMath {
 
     function _createFakeDai(address usr, uint256 wad) private {
         // Set initial balance for `usr` in the vat
-        // hevm.store(address(vat), keccak256(abi.encode(usr, 5)), bytes32(_rad(wad)));
         stdstore.target(address(vat)).sig("dai(address)").with_key(address(this)).checked_write(_rad(wad));
         // Converts the minted Dai into ERC-20 Dai and sends it to `usr`.
         daiJoin.exit(usr, wad);

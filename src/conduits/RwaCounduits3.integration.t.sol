6e041e69fddee96de24fd05d6fcba20901cae60e
Nazar Duchak <nazar@iovlabs.org>
2022-09-02 18:35:49 +0300

    feat: include fee in gem amount calculation for PSM. Add tests

--- src/conduits/RwaCounduits3.integration.t.sol
+++ src/conduits/RwaCounduits3.integration.t.sol
@@ -46,6 +46,10 @@ abstract contract RwaConduits3TestAbstract is Test, DSMath {
     uint256 GEM_DECIMALS;
     uint256 GEM_DAI_DIFF_DECIMALS;
     uint256 ART_LEFT;
+    uint256 URN_INK;
+
+    uint256 PSM_TIN;
+    uint256 PSM_TOUT;
 
     RwaInputConduit3 inputConduit;
     RwaOutputConduit3 outputConduit;
@@ -62,13 +66,37 @@ abstract contract RwaConduits3TestAbstract is Test, DSMath {
         return rad_ / 10**27;
     }
 
+    function getDaiInAmount(uint256 gemAmt) internal view returns (uint256) {
+        uint256 gemAmt18 = mul(gemAmt, GEM_DAI_DIFF_DECIMALS);
+        uint256 fee = mul(gemAmt18, PSM_TOUT) / WAD;
+        return add(gemAmt18, fee);
+    }
+
+    function getGemBuyAmount(uint256 wadAmt) internal view returns (uint256) {
+        return (wadAmt * WAD) / (WAD + PSM_TOUT) / GEM_DAI_DIFF_DECIMALS;
+    }
+
+    function getDaiOutAmount(uint256 gemAmt) internal view returns (uint256) {
+        uint256 gemAmt18 = mul(gemAmt, GEM_DAI_DIFF_DECIMALS);
+        uint256 fee = mul(gemAmt18, PSM_TIN) / WAD;
+        return sub(gemAmt18, fee);
+    }
+
     function setUp() public virtual {
-        (uint256 art, , , uint256 line, ) = vat.ilks(ILK);
         dai = DaiAbstract(DssPsm(psm).dai());
         gem = GemAbstract(AuthGemJoinAbstract(address(DssPsm(psm).gemJoin())).gem());
+
+        (uint256 art, , , uint256 line, ) = vat.ilks(ILK);
         GEM_DECIMALS = 10**uint256(gem.decimals());
         GEM_DAI_DIFF_DECIMALS = 10**uint256(dai.decimals() - gem.decimals());
         ART_LEFT = (wad(line) - art) / GEM_DAI_DIFF_DECIMALS;
+        (uint256 ink, ) = vat.urns(ILK, psm);
+        URN_INK = ink;
+
+        PSM_TIN = DssPsm(psm).tin();
+        PSM_TOUT = DssPsm(psm).tout();
+
+        deal(address(dai), me, 2 * URN_INK);
 
         inputConduit = new RwaInputConduit3(psm, testUrn);
         outputConduit = new RwaOutputConduit3(psm);
@@ -79,6 +107,10 @@ abstract contract RwaConduits3TestAbstract is Test, DSMath {
         outputConduit.kiss(me);
         outputConduit.pick(me);
     }
+
+    /*//////////////////////////////////
+               Input Conduit Tests
+    //////////////////////////////////*/
 
     function testInputConduitPush() public {
         uint256 gemAmount = ART_LEFT;
@@ -93,7 +125,7 @@ abstract contract RwaConduits3TestAbstract is Test, DSMath {
         inputConduit.push();
 
         assertEq(gem.balanceOf(address(inputConduit)), 0);
-        assertEq(dai.balanceOf(testUrn), gemAmount * GEM_DAI_DIFF_DECIMALS);
+        assertEq(dai.balanceOf(testUrn), getDaiOutAmount(gemAmount));
     }
 
     function testInputConduitPushAmountFuzz(uint256 amt) public {
@@ -109,11 +141,11 @@ abstract contract RwaConduits3TestAbstract is Test, DSMath {
         inputConduit.push(amt);
 
         assertEq(gem.balanceOf(address(inputConduit)), gemCBalanceBefore - amt);
-        assertEq(dai.balanceOf(testUrn), urnGemBalanceBefore + amt * GEM_DAI_DIFF_DECIMALS);
+        assertEq(dai.balanceOf(testUrn), urnGemBalanceBefore + getDaiOutAmount(amt));
     }
 
     function testRevertInputConduitOnSwapAboveLine() public {
-        uint256 gemAmount = ART_LEFT + 1; // 1 more then available
+        uint256 gemAmount = ART_LEFT + ART_LEFT / 5; // add 25% more then Art left to handle psm TIN fee (hopefully wont be greater then 25%)
 
         assertEq(gem.balanceOf(address(inputConduit)), 0);
 
@@ -128,6 +160,63 @@ abstract contract RwaConduits3TestAbstract is Test, DSMath {
 
         assertEq(gem.balanceOf(address(inputConduit)), gemAmount);
         assertEq(dai.balanceOf(testUrn), 0);
+    }
+
+    /*//////////////////////////////////
+               Outnput Conduit Tests
+    //////////////////////////////////*/
+
+    function testOutputConduitPush() public {
+        assertEq(outputConduit.to(), me);
+        uint256 daiAmount = URN_INK;
+
+        uint256 gemBalanceBefore = gem.balanceOf(me);
+        dai.transfer(address(outputConduit), daiAmount);
+
+        outputConduit.push();
+
+        assertEq(gem.balanceOf(address(me)), gemBalanceBefore + getGemBuyAmount(daiAmount));
+        // We lose some dust because of decimals diif dai.decimals() > gem.decimals(), which can be exited using 'quit' method
+        assertApproxEqAbs(dai.balanceOf(address(outputConduit)), 0, GEM_DAI_DIFF_DECIMALS);
+        assertEq(outputConduit.to(), address(0));
+    }
+
+    function testOutputConduitPushAmountFuzz(uint256 wadAmt) public {
+        assertEq(outputConduit.to(), me);
+
+        dai.transfer(address(outputConduit), getDaiInAmount(URN_INK / GEM_DAI_DIFF_DECIMALS));
+        uint256 cDaiBalance = dai.balanceOf(address(outputConduit));
+        uint256 gemBalance = gem.balanceOf(me);
+
+        wadAmt = bound(wadAmt, 10**18, cDaiBalance);
+
+        outputConduit.push(wadAmt);
+
+        assertEq(gem.balanceOf(me), gemBalance + getGemBuyAmount(wadAmt));
+        // We lose some dust because of decimals diif dai.decimals() > gem.decimals(), which can be exited using 'quit' method
+        assertApproxEqAbs(
+            dai.balanceOf(address(outputConduit)),
+            cDaiBalance - getDaiInAmount(getGemBuyAmount(wadAmt)),
+            GEM_DAI_DIFF_DECIMALS
+        );
+        assertEq(outputConduit.to(), address(0));
+    }
+
+    function testRevertOutputConduitOnInsufficientGemAmountInPsm() public {
+        uint256 daiAmount = URN_INK + URN_INK / 5; // add 25% more then URN Ink to handle psm TOUT fee (hopefully wont be greater then 25%)
+
+        assertEq(gem.balanceOf(address(outputConduit)), 0);
+
+        dai.transfer(address(outputConduit), daiAmount);
+
+        assertEq(dai.balanceOf(address(outputConduit)), daiAmount);
+
+        vm.expectRevert();
+        // It will revert on vat.frob()
+        // urn.ink = _add(urn.ink, dink); // _add method will revert with empty message because ink = 1000 and dink = -1100
+        outputConduit.push();
+
+        assertEq(dai.balanceOf(address(outputConduit)), daiAmount);
     }
 }
 
@@ -163,6 +252,8 @@ contract RwaConduits3PsmGUSDIntegrationTest is RwaConduits3TestAbstract {
 
     function setUp() public override {
         super.setUp();
+
+        // Add GUSD blance
         address impl = ERC20Proxy(address(gem)).erc20Impl();
         ERC20Store store = ERC20Store(ERC20Impl(impl).erc20Store());
 
@@ -174,6 +265,45 @@ contract RwaConduits3PsmGUSDIntegrationTest is RwaConduits3TestAbstract {
         vm.stopPrank();
 
         assertEq(gem.balanceOf(me), 2 * ART_LEFT);
+    }
+}
+
+contract RwaConduits3PsmGUSDWith5PercentFeeIntegrationTest is RwaConduits3TestAbstract {
+    constructor() public {
+        ILK = bytes32("PSM-GUSD-A");
+        psm = changelog.getAddress("MCD_PSM_GUSD_A");
+    }
+
+    function setUp() public override {
+        super.setUp();
+
+        // Add GUSD blance
+        address impl = ERC20Proxy(address(gem)).erc20Impl();
+        ERC20Store store = ERC20Store(ERC20Impl(impl).erc20Store());
+
+        vm.startPrank(impl);
+
+        store.setBalance(me, 2 * ART_LEFT);
+        store.setTotalSupply(gem.totalSupply() + 2 * ART_LEFT);
+
+        vm.stopPrank();
+
+        assertEq(gem.balanceOf(me), 2 * ART_LEFT);
+
+        // Adjust tin/tour for PSM
+        vm.startPrank(changelog.getAddress("MCD_PAUSE_PROXY"));
+
+        uint256 fee = (5 * WAD) / 100;
+        DssPsm(psm).file("tin", fee);
+        DssPsm(psm).file("tout", fee);
+
+        assertEq(DssPsm(psm).tin(), fee);
+        assertEq(DssPsm(psm).tout(), fee);
+
+        vm.stopPrank();
+
+        PSM_TIN = DssPsm(psm).tin();
+        PSM_TOUT = DssPsm(psm).tout();
     }
 }
 
